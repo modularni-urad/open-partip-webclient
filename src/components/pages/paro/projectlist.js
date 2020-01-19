1d5a5d12bbe64ad422b3cb78b5d576293e2fd51e
vencax <vencax@centrum.cz>
2020-01-19 07:44:16 +0100

    voting prepare

--- src/components/pages/paro/projectlist.js
+++ src/components/pages/paro/projectlist.js
@@ -1,11 +1,14 @@
-/* global axios, API */
+/* global axios, API, _, moment */
+
+// TODO: card-columns for da cards?
 
 export default {
   data: () => {
     return {
       loading: true,
       call: null,
-      projects: []
+      projects: [],
+      myvote: null
     }
   },
   created () {
@@ -21,36 +24,75 @@ export default {
       this.$data.projects = res[0].data
       this.$data.call = res[1].data[0]
       this.$data.loading = false
+      if (this.$store.state.user) {
+        const myVote = await axios.get(`${API}/paro_votes/${callId}`)
+        this.$data.myvote = _parseVote(myVote.data[0])
+      }
+    }
+  },
+  computed: {
+    canChangeProject: function () {
+      const now = moment()
+      return this.$store.state.user &&
+        now >= moment(this.$data.call.submission_start) &&
+        now <= moment(this.$data.call.thinking_start)
+    },
+    canVote: function () {
+      // TODO: check call
     }
   },
   template: `
-  <div class="container">
+  <div v-if="!loading">
 
-    <div class="row" v-if="!loading">
-      <div class="col-sm-12">
+    <div class="row">
+      <div class="col-sm-12 col-md-6">
         <h1>{{call.name}}</h1>
-
-        <router-link v-if="call.status !== 'closed'"
-          :to="{name: 'paroapply', params: {call_id: call.id}}">
-          <button class="btn btn-primary">Vytvořit / upravit projekt</button>
-        </router-link>
+      </div>
+      <div class="col-sm-12 col-md-6">
+          <router-link v-if="canChangeProject"
+            :to="{name: 'paroapply', params: {call_id: call.id}}">
+            <button class="btn btn-primary">Vytvořit / upravit projekt</button>
+          </router-link>
       </div>
     </div>
 
-    <div class="row" v-if="!loading">
+    <div class="row">
+      <div class="col-sm-12 col-md-6">
+        <div>
+          Začátek podávání návrhů: {{call.submission_start | formatDate}}<br />
+          Začátek ověřování proveditelnosti: {{call.submission_end | formatDate}}<br />
+          Začátek hlasování: {{call.voting_start | formatDate}}<br />
+          Konec hlasování: {{call.voting_end | formatDate}}
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-6">
+        <h3>Alokace: {{call.allocation}} Kč.</h3>
+        <p>Každý projekt musí získat {{call.minimum_support}}x "Líbí se mi",
+        aby postoupil do fáze ověřování proveditelnosti a poté k hlasování.</p>
+      </div>
+    </div>
+
+    <div class="row">
       <div class="col-sm-12">
         <h2>Projekty</h2>
-        <div v-if="projects.length === 0">Zatím žádné</div>
-        <div v-else class="card-group">
-          <div v-for="p in projects" class="card" style="width: 10rem;">
-            <img :src="p.photo" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">{{p.name}}</h5>
-              <p class="card-text">{{p.desc}}</p>
-              <p class="card-text">Rozpočet: {{p.total}}</p>
-              <router-link :to="{name: 'parodetail', params: {id: p.id}}">
-                <button class="btn btn-primary">Detail</button>
-              </router-link>
+      </div>
+    </div>
+
+    <div class="row d-inline-flex">
+      <div v-if="projects.length === 0">Zatím žádné</div>
+      <div v-else v-for="p in projects" class="col-sm-12 col-md-6 col-lg-3">
+        <div class="card proj">
+          <img :src="p.photo" class="card-img-top projimg" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{p.name}}</h5>
+            <p class="card-text">{{p.desc}}</p>
+            <p class="card-text">Rozpočet: {{p.total}}</p>
+            <router-link :to="{name: 'parodetail', params: {id: p.id}}">
+              <button class="btn btn-primary">Detail ...</button>
+            </router-link>
+            <div v-if="canVote" class="btn-group" role="group">
+              <button type="button" class="btn btn-success">PRO</button>
+              <button type="button" class="btn btn-danger">PROTI</button>
             </div>
           </div>
         </div>
@@ -58,4 +100,22 @@ export default {
     </div>
   </div>
   `
+}
+
+function _fillVote (arr, vote) {
+  _.each(arr, i => {
+    if (i[0] === '+') {
+      vote.pos.push(parseInt(i))
+    } else {
+      vote.neg.push(parseInt(i.substr(1)))
+    }
+  })
+}
+
+function _parseVote (content) {
+  const data = {
+    pos: [],
+    neg: []
+  }
+  return content ? _fillVote(content.split('|'), data) : data
 }
