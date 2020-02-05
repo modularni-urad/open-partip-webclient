d07b9f1dbefaa480a6b3fd79b72becf896dad02a
vencax <vencax@centrum.cz>
2020-02-05 18:02:22 +0100

    apply reworked with vuelidate

--- src/components/pages/paro/apply.js
+++ src/components/pages/paro/apply.js
@@ -1,103 +1,61 @@
-/* global axios, API */
+/* global axios, API, Vue */
+const validationMixin = window.vuelidate.validationMixin
+const validators = window.validators
 
-export default {
+export default Vue.extend({
+  mixins: [validationMixin],
   data: () => {
     return {
       working: false,
-      formValid: false,
-      model: {
-        name: '',
-        desc: '',
-        content: '',
-        budget: '',
-        photo: '',
-        total: ''
-      },
-      schema: {
-        fields: [
-          {
-            type: 'input',
-            inputType: 'text',
-            label: 'Název projektu',
-            model: 'name',
-            id: 'name',
-            placeholder: 'zadejte název projektu',
-            featured: true,
-            required: true,
-            max: 32,
-            validator: 'string'
-          },
-          {
-            type: 'input',
-            inputType: 'text',
-            label: 'Obrázek projektu. Nahrát můžete např. přes https://1iq.cz/',
-            model: 'photo',
-            id: 'photo',
-            placeholder: 'zadejte adresu obrázku'
-          },
-          {
-            type: 'textArea',
-            rows: 3,
-            label: 'Popis',
-            model: 'desc',
-            required: true,
-            hint: 'stručný popis projektu',
-            max: 128,
-            validator: 'string'
-          },
-          {
-            type: 'textArea',
-            rows: 7,
-            label: 'Úplný popis projektu včetně rozpočtu',
-            model: 'content',
-            required: true,
-            hint: 'Můžete používat markdown',
-            max: 1024,
-            validator: 'string'
-          },
-          {
-            type: 'textArea',
-            rows: 7,
-            label: 'Rozpočet projektu',
-            model: 'budget',
-            required: true,
-            hint: 'Můžete používat markdown',
-            max: 1024,
-            validator: 'string'
-          },
-          {
-            type: 'input',
-            inputType: 'number',
-            label: 'Celkové náklady s DPH',
-            model: 'total',
-            placeholder: 'zadejte číslo',
-            required: true,
-            validator: 'integer'
-          }
-        ]
-      },
-      formOptions: {
-        validateAfterLoad: true,
-        validateAfterChanged: true,
-        validateAsync: true
-      }
+      name: '',
+      desc: '',
+      content: '',
+      budget: '',
+      photo: '',
+      total: ''
+    }
+  },
+  validations: {
+    name: {
+      required: validators.required,
+      maxLength: validators.maxLength(32)
+    },
+    total: {
+      required: validators.required
+    },
+    photo: {
+      required: validators.required
+    },
+    desc: {
+      required: validators.required,
+      maxLength: validators.maxLength(128)
+    },
+    budget: {
+      required: validators.required
+    },
+    content: {
+      required: validators.required
     }
   },
   created () {
     this.fetchData()
   },
   methods: {
-    onValidated: function (isValid, errors) {
-      this.$data.formValid = isValid
-    },
     fetchData: async function () {
       this.$data.working = true
       const callId = this.$router.currentRoute.params.call_id
       const author = this.$store.state.user._id
       const url = `${API}/paro_proj/?call_id=${callId}&author=${author}`
       const res = await axios.get(url)
-      if (res.data.length > 0) this.$data.model = res.data[0]
+      if (res.data.length > 0) Object.assign(this.$data, res.data[0])
       this.$data.working = false
+    },
+    submit: function () {
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        return false
+      }
+      return this.save()
     },
     save: async function () {
       const model = this.$data.model
@@ -117,25 +75,110 @@ export default {
       }
     }
   },
-  computed: {
-    submitDisabled: function () {
-      return this.$data.formValid !== true || this.$data.working
-    }
-  },
   template: `
   <div>
     <h1>Můj projekt</h1>
     <form>
-      <vue-form-generator :schema="schema" :model="model"
-        :options="formOptions" @validated="onValidated">
-      </vue-form-generator>
+      <div class="row">
+        <div class="col">
+          <b-form-group
+            :state="!$v.name.$error"
+            label="Název projektu"
+            label-for="name-input"
+            invalid-feedback="Název je povinný a musí být maximílně 32 znaků dlouhý"
+          >
+            <b-form-input
+              id="name-input"
+              placeholder="zadejte název projektu"
+              v-model="$v.name.$model"
+              :state="!$v.name.$error"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            :state="!$v.desc.$error"
+            label="Stručný popis projektu"
+            label-for="desc-input"
+            invalid-feedback="Název je povinný a musí být maximílně 32 znaků dlouhý"
+            description="Můžete používat markdown"
+          >
+            <b-form-textarea
+              id="desc-input"
+              placeholder="stručný popis projektu"
+              v-model="$v.desc.$model"
+              :state="!$v.desc.$error"
+              rows="4"
+            ></b-form-textarea>
+          </b-form-group>
+
+          <b-form-group
+            :state="!$v.content.$error"
+            label="Úplný popis projektu"
+            label-for="content-input"
+            invalid-feedback="Název je povinný a musí být maximílně 32 znaků dlouhý"
+            description="Můžete používat markdown"
+          >
+            <b-form-textarea
+              id="content-input"
+              placeholder="Můžete používat markdown"
+              v-model="$v.content.$model"
+              :state="!$v.content.$error"
+              rows="10"
+            ></b-form-textarea>
+          </b-form-group>
+
+        </div>
+        <div class="col">
+          <b-form-group
+            :state="!$v.total.$error"
+            label="Celkové náklady s DPH"
+            label-for="total-input"
+            invalid-feedback="Toto je povinné"
+          >
+            <b-form-input
+              id="total-input"
+              type="number"
+              placeholder="zadejte číslo"
+              v-model="$v.total.$model"
+              :state="!$v.total.$error"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            :state="!$v.photo.$error"
+            label="Obrázek projektu. Nahrát můžete např. přes https://1iq.cz/"
+            label-for="photo-input"
+            invalid-feedback="Název je povinný a musí být maximílně 32 znaků dlouhý"
+          >
+            <b-form-input
+              id="photo-input"
+              placeholder="zadejte adresu obrázku"
+              v-model="$v.photo.$model"
+              :state="!$v.photo.$error"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            :state="!$v.budget.$error"
+            label="Rozpočet projektu"
+            label-for="budget-input"
+            invalid-feedback="Rozpočet projektu je povinný a musí být maximílně 128 znaků dlouhý"
+          >
+            <b-form-textarea
+              id="budget-input"
+              placeholder="Můžete používat markdown"
+              v-model="$v.budget.$model"
+              :state="!$v.budget.$error"
+              rows="10"
+            ></b-form-textarea>
+          </b-form-group>
+        </div>
+      </div>
     </form>
 
-    <button type='submit' class='btn btn-primary' v-on:click='save()'
-      v-bind:class="{disabled: submitDisabled}" :disabled="submitDisabled">
-      <b>Uložit</b>
-    </button>
-    <i class="fas fa-spinner fa-spin" v-if="working"></i>
+    <b-button class="mt-3 btn btn-primary" :disabled="$v.$anyError" @click="submit">
+      <b>Uložit</b> <i class="fas fa-spinner fa-spin" v-if="working"></i>
+    </b-button>
   </div>
   `
-}
+})
